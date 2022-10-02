6d8c392e4a26d92dade69c0ffda110c388bfa6a4
Bryan Wills <bryanwi09@gmail.com>
2022-10-02 11:45:02 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.bryanwills.dev',
+  integrations: [sitemap(), react(), image(), tailwind()]
+});
